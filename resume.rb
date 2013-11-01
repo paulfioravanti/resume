be87f13777fe13eb745bc3aa43d62a0c82a0fb23
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-11-01 20:59:35 +1100

    Clean up color methods

--- resume.rb
+++ resume.rb
@@ -14,11 +14,25 @@ DOCUMENT_NAME = "Resume"
 ################################################################################
 ### Script helper methods
 ################################################################################
-def colorize(text, color_code); "\e[#{color_code}m#{text}\e[0m"; end
-def red(text); colorize(text, 31); end
-def yellow(text); colorize(text, 33); end
-def green(text); colorize(text, 32); end
-def cyan(text); colorize(text, 36); end
+def colorize(text, color_code)
+  "\e[#{color_code}m#{text}\e[0m"
+end
+
+def red(text)
+  colorize(text, 31)
+end
+
+def yellow(text)
+  colorize(text, 33)
+end
+
+def green(text)
+  colorize(text, 32)
+end
+
+def cyan(text)
+  colorize(text, 36)
+end
 
 def permission_granted?(response)
   response.match(%r{\A(y|yes)\z}i)
