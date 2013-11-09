aebd9e1d375ee8d8167a12128224b5da647b386a
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-11-09 18:24:33 +1100

    Add specs to one-sheet resume

--- resume.rb
+++ resume.rb
@@ -999,3 +999,337 @@ end
 if __FILE__ == $0
   ResumeGenerator::CLI.new.start
 end
+
+module ResumeGenerator
+  describe CLI do
+    let(:cli) { CLI.new }
+    # stub out the innards of permission_granted? (i.e. calls chained to #gets)
+    # so it doesn't interfere with spec operation
+    let(:user_input) { double('user_input', chomp: self, match: true) }
+
+    before do
+      allow(cli).to receive(:gets).and_return(user_input)
+      allow(cli).to receive(:system).and_return # stub out `gem install ...`
+    end
+
+    describe '.report' do
+      it 'outputs the passed in message to stdout' do
+        expect(cli.class).to receive(:puts).with('hello')
+        CLI.report('hello')
+      end
+    end
+
+    describe '#start' do
+      it 'runs the script' do
+        expect(cli).to receive(:check_ability_to_generate_resume)
+        expect(cli).to receive(:generate_resume)
+        expect(cli).to receive(:clean_up)
+        cli.start
+      end
+    end
+
+    describe 'PDF generator gem installation' do
+      let(:prawn_gem) { double('prawn_gem') }
+
+      before do
+        allow(Gem::Specification).to \
+          receive(:find_by_name).with('prawn').and_return(prawn_gem)
+        allow(Gem::Version).to receive(:new).and_return(1)
+      end
+
+      context 'user has the expected gem installed' do
+        before do
+          allow(prawn_gem).to receive(:version).and_return(1)
+        end
+
+        specify 'user is not asked to install the gem' do
+          expect(cli).to_not receive(:permission_granted?)
+          cli.send(:check_ability_to_generate_resume)
+        end
+      end
+
+      context 'user has the expected gem installed, but it is an older version' do
+        before do
+          allow(prawn_gem).to receive(:version).and_return(0)
+        end
+
+        specify 'user is asked to install the gem' do
+          expect(cli).to receive(:print)
+          cli.send(:check_ability_to_generate_resume)
+        end
+      end
+
+      context 'user does not have the gem installed' do
+        before do
+          allow(Gem::Specification).to \
+            receive(:find_by_name).and_raise(Gem::LoadError)
+        end
+
+        specify 'user is asked to install the gem' do
+          expect(cli).to receive(:print)
+          cli.send(:check_ability_to_generate_resume)
+        end
+
+        context 'user agrees to install the gem' do
+          before { allow(cli).to receive(:permission_granted?).and_return(true) }
+
+          it 'executes installation' do
+            expect(cli).to receive(:install_gem)
+            cli.send(:check_ability_to_generate_resume)
+          end
+
+          context 'gem is unable to be installed' do
+            before { allow(cli).to receive(:system).and_raise }
+
+            it 'prints an error message and exits' do
+              # 'thank you', 'installing...', 'error'
+              expect(cli).to receive(:puts).exactly(3).times
+              expect(cli).to receive(:exit)
+              cli.send(:check_ability_to_generate_resume)
+            end
+          end
+        end
+
+        context 'when user does not agree to install the gem' do
+          before { allow(cli).to receive(:permission_granted?).and_return(false) }
+
+          it 'prints an error message and exits' do
+            expect(cli).to receive(:puts).once
+            expect(cli).to receive(:exit)
+            cli.send(:check_ability_to_generate_resume)
+          end
+        end
+      end
+    end
+
+    describe 'generating the PDF' do
+      before { allow(cli).to receive(:require).with('prawn') }
+
+      it 'tells the PDF to generate itself' do
+        expect(Resume).to receive(:generate)
+        cli.send(:generate_resume)
+      end
+    end
+
+    describe 'post-PDF generation' do
+      it 'shows a success message and asks to open the resume' do
+        # expect puts twice as it includes the printed message you get regardless
+        # of whether you allow the script to open resume or not
+        expect(cli).to receive(:puts).twice
+        expect(cli).to receive(:print)
+        cli.send(:clean_up)
+      end
+
+      context 'user allows the script to open the PDF' do
+        let(:document_name) { ResumeGenerator::DOCUMENT_NAME }
+
+        before { allow(cli).to receive(:permission_granted?).and_return(true) }
+
+        it 'attempts to open the document' do
+          expect(cli).to receive(:open_document)
+          cli.send(:clean_up)
+        end
+
+        context 'user is on a mac' do
+          before { stub_const('RUBY_PLATFORM', 'darwin') }
+
+          it 'opens the file using the open command' do
+            expect(cli).to receive(:system).with("open #{document_name}.pdf")
+            cli.send(:clean_up)
+          end
+        end
+
+        context 'user is on linux' do
+          before { stub_const('RUBY_PLATFORM', 'linux') }
+
+          it 'opens the file using the xdg-open command' do
+            expect(cli).to receive(:system).with("xdg-open #{document_name}.pdf")
+            cli.send(:clean_up)
+          end
+        end
+
+        context 'user is on windows' do
+          before { stub_const('RUBY_PLATFORM', 'windows') }
+
+          it 'opens the file using the cmd command' do
+            expect(cli).to \
+              receive(:system).with("cmd /c \"start #{document_name}.pdf\"")
+            cli.send(:clean_up)
+          end
+        end
+
+        context 'user is on an unknown operating system' do
+          before { stub_const('RUBY_PLATFORM', 'unknown') }
+
+          it 'prints a message telling the user to open the file' do
+            # including calls to #puts in #clean_up
+            expect(cli).to receive(:puts).exactly(3).times
+            cli.send(:clean_up)
+          end
+        end
+      end
+
+      context 'user does not allow script to open PDF' do
+        before { allow(cli).to receive(:permission_granted?).and_return(false) }
+
+        it 'does not attempt to open the document' do
+          expect(cli).to_not receive(:open_document)
+          cli.send(:clean_up)
+        end
+      end
+    end
+  end
+
+  describe Decodable do
+    let(:decoder) { Object.new.extend(Decodable) }
+
+    describe '.d' do
+      it 'wraps the Base64.strict_decode64 method' do
+        expect(Base64).to receive(:strict_decode64).with('Hello')
+        decoder.d('Hello')
+      end
+    end
+  end
+
+  describe Image do
+    describe '.for' do
+      let(:resources) do
+        [
+          'background',
+          'email',
+          'linked_in',
+          'github',
+          'stackoverflow',
+          'speakerdeck',
+          'vimeo',
+          'code_school',
+          'twitter',
+          'blog',
+          'rc',
+          'ruby',
+          'rails',
+          'gw',
+          'rnt',
+          'sra',
+          'jet',
+          'satc',
+          'mit',
+          'bib',
+          'ryu',
+          'tafe'
+        ]
+      end
+      let(:image) { double('image') }
+
+      before do
+        allow(Image).to receive(:open).and_return(image)
+      end
+
+      it 'returns the image for a resource' do
+        resources.each do |resource|
+          expect(Image.for(resource)).to equal(image)
+        end
+      end
+    end
+  end
+
+  describe Link do
+    describe '.for' do
+      let(:resources) do
+        [
+          'email',
+          'linked_in',
+          'github',
+          'stackoverflow',
+          'speakerdeck',
+          'vimeo',
+          'code_school',
+          'twitter',
+          'blog',
+          'rc',
+          'ruby',
+          'rails',
+          'gw',
+          'rnt',
+          'sra',
+          'jet',
+          'satc',
+          'mit',
+          'bib',
+          'ryu',
+          'tafe'
+        ]
+      end
+      let(:decoded_email) { double('decoded_email') }
+
+      before do
+        allow(Link).to receive(:d).and_return(decoded_email)
+      end
+
+      it 'returns the link for a resource' do
+        resources.each do |resource|
+          expect(Link.for(resource)).to equal(decoded_email)
+        end
+      end
+    end
+  end
+
+  describe Resource do
+    describe '.for' do
+      let(:image) { double('image') }
+      let(:link) { double('link') }
+      let(:name) { 'email' }
+      let(:resource) { Resource.for(name) }
+
+      before do
+        allow(Image).to receive(:for).with(name.to_sym).and_return(image)
+        allow(Link).to receive(:for).with(name.to_sym).and_return(link)
+      end
+
+      it 'has an image' do
+        expect(resource.image).to equal(image)
+      end
+
+      it 'has a link' do
+        expect(resource.link).to equal(link)
+      end
+    end
+  end
+
+  describe ResumeGenerator do
+    describe 'constants' do
+      let(:version) { ResumeGenerator.const_get('VERSION') }
+      let(:document_name) { ResumeGenerator.const_get('DOCUMENT_NAME') }
+
+      it 'has a VERSION constant' do
+        expect(version).to_not be_empty
+      end
+
+      it 'has a DOCUMENT_NAME constant' do
+        expect(document_name).to_not be_empty
+      end
+    end
+  end
+
+  describe Resume do
+    require 'prawn'
+
+    describe ".generate" do
+      # Link points to a 1x1 pixel placeholder to not slow down test suite
+      let(:placeholder_image) do
+        open('http://farm4.staticflickr.com/3722/10753699026_a1603247cf_m.jpg')
+      end
+      let(:filename) { "#{ResumeGenerator::DOCUMENT_NAME}.pdf" }
+
+      before do
+        allow(Image).to receive(:for).with(anything).and_return(placeholder_image)
+        Resume.generate
+      end
+      after { File.delete(filename) }
+
+      it 'generates a pdf resume' do
+        expect(File.exist?(filename)).to be_true
+      end
+    end
+  end
+end
