7f662b1aff10775338f23855d2fa1289c65eb3ff
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-11-09 17:34:58 +1100

    Add spec for resume

---
+++ spec/resume_spec.rb
@@ -0,0 +1,21 @@
+require 'prawn'
+
+describe Resume do
+  describe ".generate" do
+    # Link points to a 1x1 pixel placeholder to not slow down test suite
+    let(:placeholder_image) do
+      open('http://farm4.staticflickr.com/3722/10753699026_a1603247cf_m.jpg')
+    end
+    let(:filename) { "#{Resume::CLI::DOCUMENT_NAME}.pdf" }
+
+    before do
+      allow(Image).to receive(:for).with(anything).and_return(placeholder_image)
+      Resume.generate
+    end
+    after { File.delete(filename) }
+
+    it 'generates a pdf resume' do
+      expect(File.exist?(filename)).to be_true
+    end
+  end
+end
