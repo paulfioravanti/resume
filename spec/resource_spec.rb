2e86bcca7f934ad97c2365600673c6a2f9e525c5
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-11-17 18:55:01 +1100

    Remove extraneous specs and test .background_image method

--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -1,21 +1,72 @@
+require 'spec_helper'
+
 describe Resource do
   describe '.for' do
     let(:image) { double('image') }
-    let(:link) { double('link') }
-    let(:name) { 'email' }
-    let(:resource) { Resource.for(name) }
+    let(:hash) do
+      {
+        image: "http://farm.staticflickr.com/example.jpg",
+        link: "d3d3LmV4YW1wbGUuY29t",
+        width: 35,
+        height: 35,
+        fit: [35, 35],
+        align: "center",
+        move_up: 35,
+        bars: 3,
+        size: 40,
+        origin: 415,
+        at: 280
+      }
+    end
+    let(:resource) { Resource.for(hash) }
 
     before do
-      allow(Image).to receive(:for).with(name.to_sym).and_return(image)
-      allow(Link).to receive(:for).with(name.to_sym).and_return(link)
+      allow(Resource).to receive(:open).with(hash[:image]).and_return(image)
     end
 
     it 'has an image' do
-      expect(resource.image).to equal(image)
+      expect(resource.image).to eq(image)
     end
 
     it 'has a link' do
-      expect(resource.link).to equal(link)
+      expect(resource.link).to eq('www.example.com')
     end
+
+    it 'has a width' do
+      expect(resource.width).to eq(35)
+    end
+
+    it 'has a height' do
+      expect(resource.height).to eq(35)
+    end
+
+    it 'has a fit' do
+      expect(resource.fit).to eq([35, 35])
+    end
+
+    it 'has an align' do
+      expect(resource.align).to eq(:center)
+    end
+
+    it 'has a move_up' do
+      expect(resource.move_up).to eq(35)
+    end
+
+    it 'has a bars' do
+      expect(resource.bars).to eq(3)
+    end
+
+    it 'has a size' do
+      expect(resource.size).to eq(40)
+    end
+
+    it 'has an origin' do
+      expect(resource.origin).to eq(415)
+    end
+
+    it 'has an at ' do
+      expect(resource.at).to eq(280)
+    end
+
   end
 end
--- spec/resume_spec.rb
+++ spec/resume_spec.rb
@@ -1,22 +1,36 @@
 require 'prawn'
 
 describe Resume do
+  # Link points to a 1x1 pixel placeholder to not slow down test suite
+  # Couldn't send Prawn::Document an image test double
+  let(:placeholder_image) do
+    open('http://farm4.staticflickr.com/3722/10753699026_a1603247cf_m.jpg')
+  end
+
   describe ".generate" do
-    # Link points to a 1x1 pixel placeholder to not slow down test suite
-    # Couldn't send Prawn::Document an image test double
-    let(:placeholder_image) do
-      open('http://farm4.staticflickr.com/3722/10753699026_a1603247cf_m.jpg')
-    end
     let(:filename) { "#{ResumeGenerator::DOCUMENT_NAME}.pdf" }
 
     before do
-      allow(Image).to receive(:for).with(anything).and_return(placeholder_image)
+      allow(Resume).to receive(:background_image).and_return(placeholder_image)
+      allow(Resource).to \
+        receive(:open).with(anything).and_return(placeholder_image)
       Resume.generate
     end
-    after { File.delete(filename) }
+    # after { File.delete(filename) }
 
     it 'generates a pdf resume' do
       expect(File.exist?(filename)).to be_true
+    end
+  end
+
+  describe ".background_image" do
+    before do
+      allow(Resume).to \
+        receive(:open).with(anything).and_return(placeholder_image)
+    end
+
+    it 'has a background image' do
+      expect(Resume.background_image).to eq(placeholder_image)
     end
   end
 end
