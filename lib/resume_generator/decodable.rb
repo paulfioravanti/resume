d3d94f248fe1bf8149ce0ad5c27b169d6bbaa8b4
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-08-24 18:45:18 +1000

    Add comments

--- lib/resume_generator/decodable.rb
+++ lib/resume_generator/decodable.rb
@@ -2,7 +2,9 @@ require 'base64'
 
 module ResumeGenerator
   module Decodable
-    def d(string) # decode string
+    # This is just a helper method due to the sheer amount of decoding that
+    # occurs throughout the code
+    def d(string)
       Base64.strict_decode64(string)
     end
   end
