af7cb055a127fe6544d2a97c7c8b7d33984f6b89
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-08-29 23:58:56 +1000

    Extract listing out

---
+++ lib/resume_generator/listing.rb
@@ -0,0 +1,138 @@
+require 'decodable'
+require 'resource'
+
+module ResumeGenerator
+  class Listing
+    include Decodable
+
+    attr_reader :pdf, :data
+
+    def self.generate(pdf, data)
+      new(pdf, data).generate
+    end
+
+    def initialize(pdf, data)
+      @pdf = pdf
+      @data = data
+    end
+
+    def generate
+      header
+      logo_link
+      details if data.has_key?(:summary)
+    end
+
+    private
+
+    def header
+      pdf.move_down data[:y_header_start] || 15
+      if data[:at]
+        text_box_header
+      else
+        text_header
+      end
+    end
+
+    def details
+      pdf.move_down data[:y_details_start] || 10
+      summary(data[:summary])
+      profile(data[:profile])
+    end
+
+    def summary(string)
+      pdf.text(d(string), inline_format: true)
+    end
+
+    def profile(items)
+      return unless items
+      table_data = items.reduce([]) do |data, item|
+        data << ['•', d(item)]
+      end
+      pdf.table(table_data, cell_style: { borders: [], height: 21 })
+    end
+
+    def text_header
+      formatted_text_entry_for(d(data[:position]), 12)
+      formatted_text_entry_for(d(data[:organisation]), 11)
+      formatted_text_period_and_location(
+        d(data[:period]),
+        d(data[:location][:name]),
+        d(data[:location][:link])
+      )
+    end
+
+    def text_box_header
+      at = data[:at]
+      formatted_text_box_entry_for(d(data[:position]), 12, at, 14)
+      formatted_text_box_entry_for(d(data[:organisation]), 11, at, 13)
+      formatted_text_box_period_and_location(
+        d(data[:period]),
+        d(data[:location][:name]),
+        d(data[:location][:link]),
+        at
+      )
+    end
+
+    def logo_link
+      logo = Resource.for(data[:logo].merge(at: data[:at]))
+      pdf.move_up data[:y_logo_start] || 40
+      pdf.bounding_box([logo.origin, pdf.cursor],
+        width: logo.width, height: logo.height) do
+        pdf.image(logo.image, fit: logo.fit, align: logo.align)
+        pdf.move_up logo.move_up
+        transparent_link(logo)
+      end
+    end
+
+    def transparent_link(resource)
+      pdf.transparent(0) do
+        pdf.formatted_text(
+          [
+            {
+              text: '|' * resource.bars,
+              size: resource.size,
+              link: resource.link
+            }
+          ], align: resource.align
+        )
+      end
+    end
+
+    def formatted_text_entry_for(item, size)
+      pdf.formatted_text(
+        [formatted_entry_args_for(item, size)]
+      )
+    end
+
+    def formatted_text_box_entry_for(item, size, at, value)
+      pdf.formatted_text_box(
+        [formatted_entry_args_for(item, size)], at: [at, pdf.cursor]
+      )
+      pdf.move_down value
+    end
+
+    def formatted_entry_args_for(string, size)
+      { text: string, styles: [:bold], size: size }
+    end
+
+    def formatted_text_period_and_location(period, name, link)
+      pdf.formatted_text(
+        period_and_location_args_for(period, name, link)
+      )
+    end
+
+    def formatted_text_box_period_and_location(period, name, link, at)
+      pdf.formatted_text_box(
+        period_and_location_args_for(period, name, link),
+        at: [at, pdf.cursor]
+      )
+    end
+
+    def period_and_location_args_for(period, name, link)
+      [
+        { text: period, color: '666666', size: 10 },
+        { text: name, link: link, color: '666666', size: 10 }
+      ]
+    end
+  end
+end
--- lib/resume_generator/resume_helper.rb
+++ lib/resume_generator/resume_helper.rb
@@ -2,10 +2,11 @@ require 'json'
 require 'open-uri'
 require 'decodable'
 require 'social_media_icon_set'
+require 'listing'
 
 module ResumeGenerator
   module ResumeHelper
-    include Decodable#, SocialMediaIconSet
+    include Decodable
 
     RESUME = JSON.parse(
       open('resources/resume.json').read,
@@ -59,7 +60,7 @@ module ResumeGenerator
       heading d('RW1wbG95bWVudCBIaXN0b3J5')
       entries = RESUME[:entries]
       [:rc, :fl, :gw, :rnt, :sra, :jet, :satc].each do |entry|
-        listing_for(entries[entry])
+        Listing.generate(self, entries[entry])
       end
       move_down 10
       stroke_horizontal_rule { color '666666' }
@@ -69,94 +70,7 @@ module ResumeGenerator
       heading d('RWR1Y2F0aW9u')
       entries = RESUME[:entries]
       [:mit, :bib, :ryu, :tafe].each do |entry|
-        listing_for(entries[entry])
-      end
-    end
-
-    # def showcase_table_for(skills)
-    #   table_data = skills[:content].reduce([]) do |data, entry|
-    #     data << [d(entry.first), d(entry.last)]
-    #   end
-    #   table(table_data, skills[:properties])
-    # end
-
-    def listing_for(entry)
-      header_for(entry)
-      logo_link_for(entry)
-      details_for(entry) if entry.has_key?(:summary)
-    end
-
-    def header_for(entry)
-      move_down entry[:y_header_start] || 15
-      if entry[:at]
-        text_box_header_for(entry)
-      else
-        text_header_for(entry)
-      end
-    end
-
-    def details_for(entry)
-      move_down entry[:y_details_start] || 10
-      summary(entry[:summary])
-      profile(entry[:profile])
-    end
-
-    def summary(string)
-      text(d(string), inline_format: true)
-    end
-
-    def profile(items)
-      return unless items
-      table_data = items.reduce([]) do |data, item|
-        data << ['•', d(item)]
-      end
-      table(table_data, cell_style: { borders: [], height: 21 })
-    end
-
-    def text_header_for(entry)
-      formatted_text_entry_for(d(entry[:position]), 12)
-      formatted_text_entry_for(d(entry[:organisation]), 11)
-      formatted_text_period_and_location(
-        d(entry[:period]),
-        d(entry[:location][:name]),
-        d(entry[:location][:link])
-      )
-    end
-
-    def text_box_header_for(entry)
-      at = entry[:at]
-      formatted_text_box_entry_for(d(entry[:position]), 12, at, 14)
-      formatted_text_box_entry_for(d(entry[:organisation]), 11, at, 13)
-      formatted_text_box_period_and_location(
-        d(entry[:period]),
-        d(entry[:location][:name]),
-        d(entry[:location][:link]),
-        at
-      )
-    end
-
-    def logo_link_for(entry)
-      logo = Resource.for(entry[:logo].merge(at: entry[:at]))
-      move_up entry[:y_logo_start] || 40
-      bounding_box([logo.origin, cursor],
-        width: logo.width, height: logo.height) do
-        image logo.image, fit: logo.fit, align: logo.align
-        move_up logo.move_up
-        transparent_link(logo)
-      end
-    end
-
-    def transparent_link(resource)
-      transparent(0) do
-        formatted_text(
-          [
-            {
-              text: '|' * resource.bars,
-              size: resource.size,
-              link: resource.link
-            }
-          ], align: resource.align
-        )
+        Listing.generate(self, entries[entry])
       end
     end
 
@@ -171,43 +85,6 @@ module ResumeGenerator
           }
         ]
       )
-    end
-
-    def formatted_text_entry_for(item, size)
-      formatted_text(
-        [formatted_entry_args_for(item, size)]
-      )
-    end
-
-    def formatted_text_box_entry_for(item, size, at, value)
-      formatted_text_box(
-        [formatted_entry_args_for(item, size)], at: [at, cursor]
-      )
-      move_down value
-    end
-
-    def formatted_entry_args_for(string, size)
-      { text: string, styles: [:bold], size: size }
-    end
-
-    def formatted_text_period_and_location(period, name, link)
-      formatted_text(
-        period_and_location_args_for(period, name, link)
-      )
-    end
-
-    def formatted_text_box_period_and_location(period, name, link, at)
-      formatted_text_box(
-        period_and_location_args_for(period, name, link),
-        at: [at, cursor]
-      )
-    end
-
-    def period_and_location_args_for(period, name, link)
-      [
-        { text: period, color: '666666', size: 10 },
-        { text: name, link: link, color: '666666', size: 10 }
-      ]
     end
   end
 end
