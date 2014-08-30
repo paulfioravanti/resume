dd31661b80727db61f2385a9754fb7e8891b7393
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-08-30 12:35:52 +1000

    Attempt to refactor out some complexity

--- lib/resume_generator/logo_link.rb
+++ lib/resume_generator/logo_link.rb
@@ -8,23 +8,31 @@ module ResumeGenerator
     attr_reader :pdf, :logo, :y_logo_start
 
     def self.generate(pdf, data)
-      new(pdf, data).generate
+      logo = Resource.for(data[:logo].merge(at: data[:at]))
+      y_logo_start = data[:y_logo_start] || 40
+      new(pdf, logo, y_logo_start).generate
     end
 
-    def initialize(pdf, data)
+    def initialize(pdf, logo, y_logo_start)
       @pdf = pdf
-      @logo = Resource.for(data[:logo].merge(at: data[:at]))
-      @y_logo_start = data[:y_logo_start] || 40
+      @logo = logo
+      @y_logo_start = y_logo_start
     end
 
     def generate
       pdf.move_up y_logo_start
       pdf.bounding_box([logo.origin, pdf.cursor],
         width: logo.width, height: logo.height) do
-        pdf.image(logo.image, fit: logo.fit, align: logo.align)
+        render_image
         pdf.move_up logo.move_up
         transparent_link(pdf, logo)
       end
+    end
+
+    private
+
+    def render_image
+      pdf.image(logo.image, fit: logo.fit, align: logo.align)
     end
   end
 end
