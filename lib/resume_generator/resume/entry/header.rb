0daa80be476ca83f277c5192af6f81b95c665b3e
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-04-30 09:26:16 +1000

    Collapse different types of text entries/text box entries into the header class

--- lib/resume_generator/resume/entry/header.rb
+++ lib/resume_generator/resume/entry/header.rb
@@ -5,6 +5,7 @@ module ResumeGenerator
   module Resume
     module Entry
       class Header
+        include Decoder
 
         attr_reader :pdf, :position, :organisation,
                     :period, :location, :at_x_position
@@ -27,20 +28,55 @@ module ResumeGenerator
           @period = period
           @location = location
           @at_x_position = at_x_position
-          # Different rendering behaviour needed depending on whether the header is
-          # being drawn from left to right on the page or specifically placed at
-          # a location
-          if at_x_position
-            extend FormattedTextBoxEntry
-          else
-            extend FormattedTextEntry
-          end
         end
 
         def generate
-          generate_position
-          generate_organisation
-          generate_period_and_location
+          # Different rendering behaviour needed depending on whether the header
+          # is being drawn from left to right on the page or specifically placed
+          # at a location
+          if at_x_position
+            formatted_text_box_header
+          else
+            formatted_text_header
+          end
+        end
+
+        private
+
+        def formatted_text_header
+          pdf.formatted_text([properties_for(position)])
+          pdf.formatted_text([properties_for(organisation)])
+          pdf.formatted_text([
+            properties_for(period),
+            properties_for(location)
+          ])
+        end
+
+        def formatted_text_box_header
+          pdf.formatted_text_box(
+            [properties_for(position)],
+            at: [at_x_position, pdf.cursor]
+          )
+          pdf.move_down position[:bottom_padding]
+          pdf.formatted_text_box(
+            [properties_for(organisation)],
+            at: [at_x_position, pdf.cursor]
+          )
+          pdf.move_down organisation[:bottom_padding]
+          pdf.formatted_text_box(
+            [properties_for(period), properties_for(location)],
+            at: [at_x_position, pdf.cursor]
+          )
+        end
+
+        def properties_for(section)
+          {
+            text: d(section[:text]),
+            styles: section[:styles].map(&:to_sym),
+            size: section[:size],
+            color: section[:colour],
+            link: d(section[:link]),
+          }
         end
       end
     end
