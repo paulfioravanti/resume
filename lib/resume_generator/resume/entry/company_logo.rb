2dacb27b424d53589d082e5e70664c344e59fcf4
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-05-01 23:37:55 +1000

    Attempt to extract some complexity into another method

--- lib/resume_generator/resume/entry/company_logo.rb
+++ lib/resume_generator/resume/entry/company_logo.rb
@@ -11,21 +11,26 @@ module ResumeGenerator
           new(pdf, logo).generate
         end
 
+        def generate
+          pdf.move_up logo.y_start
+          pdf.bounding_box(
+            [logo.origin, pdf.cursor], width: logo.width, height: logo.height
+          ) do
+            render_image_link
+          end
+        end
+
+        private
+
         def initialize(pdf, logo)
           @pdf = pdf
           @logo = logo
         end
 
-        def generate
-          pdf.move_up logo.y_start
-          pdf.bounding_box(
-            [logo.origin, pdf.cursor],
-            width: logo.width, height: logo.height
-          ) do
-            pdf.image(logo.image, fit: logo.fit, align: logo.align)
-            pdf.move_up logo.link_overlay_start
-            transparent_link(pdf, logo)
-          end
+        def render_image_link
+          pdf.image(logo.image, fit: logo.fit, align: logo.align)
+          pdf.move_up logo.link_overlay_start
+          transparent_link(pdf, logo)
         end
       end
     end
