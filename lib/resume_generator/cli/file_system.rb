be505cf6b78e072dfaeb7ccf9890911a4d4eef98
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-05-06 09:00:07 +1000

    Refactor and spec out FileSystem class

--- lib/resume_generator/cli/file_system.rb
+++ lib/resume_generator/cli/file_system.rb
@@ -4,11 +4,11 @@ module ResumeGenerator
       def self.open_document(app)
         case RUBY_PLATFORM
         when %r(darwin)
-          system("open #{app.filename}")
+          system('open', app.filename)
         when %r(linux)
-          system("xdg-open #{app.filename}")
+          system('xdg-open', app.filename)
         when %r(windows)
-          system("cmd /c \"start #{app.filename}\"")
+          system('cmd', '/c', "\"start #{app.filename}\"")
         else
           app.request_user_to_open_document
         end
--- spec/cli/file_system_spec.rb
+++ spec/cli/file_system_spec.rb
@@ -1,40 +1,56 @@
-  context 'user is on a mac' do
-    # before { stub_const('RUBY_PLATFORM', 'darwin') }
+require 'spec_helper'
+require 'resume_generator/cli/file_system'
 
-    it 'opens the file using the open command' do
-      # expect(cli).to \
-      #   receive(:system).with("open #{document_name}.pdf")
-      # cli.send(:clean_up)
+RSpec.describe ResumeGenerator::CLI::FileSystem do
+  describe '.open_document' do
+    let(:resume_name) { 'My Resume.pdf' }
+    let(:app) { double('app', filename: resume_name) }
+
+    context 'when run on Mac OS' do
+      let(:mac_open_file_args) { ['open', app.filename] }
+
+      before { stub_const('RUBY_PLATFORM', 'darwin') }
+
+      it 'opens the file using the open command' do
+        expect(described_class).to \
+          receive(:system).with(*mac_open_file_args)
+        described_class.open_document(app)
+      end
+    end
+
+    context 'when run on Linux' do
+      let(:linux_open_file_args) { ['xdg-open', app.filename] }
+
+      before { stub_const('RUBY_PLATFORM', 'linux') }
+
+      it 'opens the file using the xdg-open command' do
+        expect(described_class).to \
+          receive(:system).with(*linux_open_file_args)
+        described_class.open_document(app)
+      end
+    end
+
+    context 'when run on Windows' do
+      let(:windows_open_file_args) do
+        ['cmd', '/c', "\"start #{app.filename}\""]
+      end
+
+      before { stub_const('RUBY_PLATFORM', 'windows') }
+
+      it 'opens the file using the cmd /c command' do
+        expect(described_class).to \
+          receive(:system).with(*windows_open_file_args)
+        described_class.open_document(app)
+      end
+    end
+
+    context 'when run on an unknown operating system' do
+      before { stub_const('RUBY_PLATFORM', 'unknown') }
+
+      it 'requests the user to open the document themself' do
+        expect(app).to receive(:request_user_to_open_document)
+        described_class.open_document(app)
+      end
     end
   end
-
-  context 'user is on linux' do
-    # before { stub_const('RUBY_PLATFORM', 'linux') }
-
-    it 'opens the file using the xdg-open command' do
-    #   expect(cli).to \
-    #     receive(:system).with("xdg-open #{document_name}.pdf")
-    #   cli.send(:clean_up)
-    end
-  end
-
-  context 'user is on windows' do
-    # before { stub_const('RUBY_PLATFORM', 'windows') }
-
-    it 'opens the file using the cmd command' do
-    #   expect(cli).to \
-    #     receive(:system).
-    #       with("cmd /c \"start #{document_name}.pdf\"")
-    #   cli.send(:clean_up)
-    end
-  end
-
-  context 'user is on an unknown operating system' do
-    # before { stub_const('RUBY_PLATFORM', 'unknown') }
-
-    it 'prints a message telling the user to open the file' do
-    #   expect(cli).to \
-    #     receive(:request_user_to_open_document).and_call_original
-    #   cli.send(:clean_up)
-    end
-  end
+end
