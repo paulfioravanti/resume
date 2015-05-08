c7ac796d244b71f1d62c4d843659d97434a623f5
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-05-08 15:31:40 +1000

    Remove explicit nil check

--- one_sheet/resume.rb
+++ one_sheet/resume.rb
@@ -403,10 +403,9 @@ module ResumeGenerator
     # This is just a helper method due to the sheer amount of decoding that
     # occurs throughout the code
     def d(string)
-      return if string.nil?
       # Force encoding to UTF-8 is needed for strings that had UTF-8 characters
       # in them when they were originally encoded
-      Base64.strict_decode64(string).force_encoding('utf-8')
+      Base64.strict_decode64(string).force_encoding('utf-8') if string
     end
   end
 
