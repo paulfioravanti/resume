13dcb391bb29963d1dc1c5ee5e0db2365688ab91
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-05-08 15:54:43 +1000

    Rename the d method to decode and just have 'd' be an alias for it

--- one_sheet/resume.rb
+++ one_sheet/resume.rb
@@ -402,11 +402,12 @@ module ResumeGenerator
     end
     # This is just a helper method due to the sheer amount of decoding that
     # occurs throughout the code
-    def d(string)
+    def decode(string)
       # Force encoding to UTF-8 is needed for strings that had UTF-8 characters
       # in them when they were originally encoded
       Base64.strict_decode64(string).force_encoding('utf-8') if string
     end
+    alias_method :d, :decode
   end
 
   module Resume
