763e78e5c8b2220411320b13b8ffea7bbe771c16
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-05-08 15:27:52 +1000

    Remove multiple callings of app.filename

--- lib/resume_generator/cli/file_system.rb
+++ lib/resume_generator/cli/file_system.rb
@@ -2,13 +2,14 @@ module ResumeGenerator
   module CLI
     class FileSystem
       def self.open_document(app)
+        filename = app.filename
         case RUBY_PLATFORM
         when %r(darwin)
-          system('open', app.filename)
+          system('open', filename)
         when %r(linux)
-          system('xdg-open', app.filename)
+          system('xdg-open', filename)
         when %r(windows)
-          system('cmd', '/c', "\"start #{app.filename}\"")
+          system('cmd', '/c', "\"start #{filename}\"")
         else
           app.request_user_to_open_document
         end
