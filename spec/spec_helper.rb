4f36e3c68f237aeea0377805957d627c17c9d04c
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-06-13 11:15:41 +1000

    If the user does not have an internet connection, don't allow the specs to run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
     begin
       require 'prawn'
       require 'prawn/table'
+      open('http://farm4.staticflickr.com/3722/10753699026_a1603247cf_m.jpg')
     rescue LoadError
       puts red(
         'You need to have the prawn and prawn-table gems installed in '\
@@ -24,6 +25,14 @@ RSpec.configure do |config|
       puts yellow(
         'Either install them yourself or run the resume and it will '\
         'install them for you.'
+      )
+      exit
+    rescue OpenURI::HTTPError
+      puts red(
+        'You need to have an internet connection in order to run the specs.'
+      )
+      puts yellow(
+        'Please ensure that you have one before running the specs.'
       )
       exit
     end
