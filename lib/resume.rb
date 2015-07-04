1d7ee0afde154772277774e7e4042c255d999f12
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-07-04 14:18:08 +1000

    Add pry-byebug

--- lib/resume.rb
+++ lib/resume.rb
@@ -1,5 +1,11 @@
 module Resume
   VERSION = '0.6'
   DATA_LOCATION = 'resources/'
+
+  begin
+    require 'pry-byebug'
+  rescue LoadError
+    # Ignore requiring gems that are just used for development
+  end
 end
 
