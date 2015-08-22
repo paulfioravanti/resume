b2433484abd943c84f64e7b1e0254645318e4ea5
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-08-22 13:34:20 +1000

    Scope specs to their modules in anticipation of making the specs more portable to the one-sheet resume

--- spec/lib/i18n/core_ext_spec.rb
+++ spec/lib/i18n/core_ext_spec.rb
@@ -1,37 +1,39 @@
 require 'spec_helper'
 require 'i18n/core_ext'
 
-RSpec.describe I18n::Backend::Base do
-  # Since #load_erb is a protected method, this spec tests the
-  # interface that calls it: #load_translations
-  describe '#load_translations' do
-    let(:translations) do
-      I18n.backend.instance_variable_get(:@translations)
-    end
+module I18n
+  module Backend
+    RSpec.describe Base do
+      # Since #load_erb is a protected method, this spec tests the
+      # interface that calls it: #load_translations
+      describe '#load_translations' do
+        let(:translations) do
+          I18n.backend.instance_variable_get(:@translations)
+        end
 
-    before do
-      I18n.backend = I18n::Backend::Simple.new
-    end
+        before { I18n.backend = Simple.new }
 
-    context 'when loading data from a yml.erb file' do
-      let(:filename) { 'en.yml.erb' }
-      let(:yaml) do
-        <<-YAML
-          en:
-            foo: 1 + 1 = <%= 1 + 1 %>
-        YAML
-      end
-      let(:erb_interpolated_hash) do
-        { en: { foo: '1 + 1 = 2' } }
-      end
+        context 'when loading data from a yml.erb file' do
+          let(:filename) { 'en.yml.erb' }
+          let(:yaml) do
+            <<-YAML
+              en:
+                foo: 1 + 1 = <%= 1 + 1 %>
+            YAML
+          end
+          let(:erb_interpolated_hash) do
+            { en: { foo: '1 + 1 = 2' } }
+          end
 
-      before do
-        allow(File).to receive(:read).with(filename).and_return(yaml)
-        I18n.backend.load_translations(filename)
-      end
+          before do
+            allow(File).to receive(:read).with(filename).and_return(yaml)
+            I18n.backend.load_translations(filename)
+          end
 
-      it 'loads the data into a hash and interpolates the ERB' do
-        expect(translations).to eq(erb_interpolated_hash)
+          it 'loads the data into a hash and interpolates the ERB' do
+            expect(translations).to eq(erb_interpolated_hash)
+          end
+        end
       end
     end
   end
--- spec/lib/resume/output_spec.rb
+++ spec/lib/resume/output_spec.rb
@@ -1,133 +1,135 @@
 require 'spec_helper'
 require 'resume/output'
 
-RSpec.describe Resume::Output do
-  shared_context 'I18n keys' do |colour|
-    let(:key) { :message }
-    let(:params_to_interpolate) { { param: 'param' } }
-    let(:params) { [key, params_to_interpolate] }
-    let(:translated_message) { 'Translated Message' }
-    let(:colourised_translated_message) do
-      "Colourised #{translated_message}"
-    end
-    let(:colourised_translated_message_with_new_line) do
-      "#{colourised_translated_message}\n"
-    end
-
-    before do
-      allow(I18n).to receive(:translate).
-        with(key, params_to_interpolate).and_return(translated_message)
-      allow(described_class).to \
-        receive(colour).with(translated_message).
-          and_return(colourised_translated_message)
-    end
-  end
-
-  describe '.messages' do
-    let(:messages) { { error: :foo, warning: :bar } }
-
-    it "calls to output each message as its specific type" do
-      messages.each do |type, key|
-        expect(described_class).to receive(type).with(key)
+module Resume
+  RSpec.describe Output do
+    shared_context 'I18n keys' do |colour|
+      let(:key) { :message }
+      let(:params_to_interpolate) { { param: 'param' } }
+      let(:params) { [key, params_to_interpolate] }
+      let(:translated_message) { 'Translated Message' }
+      let(:colourised_translated_message) do
+        "Colourised #{translated_message}"
       end
-      described_class.messages(messages)
-    end
-  end
+      let(:colourised_translated_message_with_new_line) do
+        "#{colourised_translated_message}\n"
+      end
 
-  describe '.error' do
-    include_context 'I18n keys', :red
-
-    let(:outputting_the_error) { -> { described_class.error(params) } }
-
-    it 'outputs the values of the I18n-ised error to stdout' do
-      expect(outputting_the_error).to \
-        output(colourised_translated_message_with_new_line).to_stdout
-    end
-  end
-
-  describe '.warning' do
-    include_context 'I18n keys', :yellow
-
-    let(:outputting_the_warning) do
-      -> { described_class.warning(params) }
+      before do
+        allow(I18n).to receive(:translate).
+          with(key, params_to_interpolate).and_return(translated_message)
+        allow(described_class).to \
+          receive(colour).with(translated_message).
+            and_return(colourised_translated_message)
+      end
     end
 
-    it 'outputs the values of the I18n-ised warning to stdout' do
-      expect(outputting_the_warning).to \
-        output(colourised_translated_message_with_new_line).to_stdout
-    end
-  end
+    describe '.messages' do
+      let(:messages) { { error: :foo, warning: :bar } }
 
-  describe '.question' do
-    include_context 'I18n keys', :yellow
-
-    let(:outputting_the_question) do
-      -> { described_class.question(params) }
+      it "calls to output each message as its specific type" do
+        messages.each do |type, key|
+          expect(described_class).to receive(type).with(key)
+        end
+        described_class.messages(messages)
+      end
     end
 
-    it 'outputs the values of the I18n-ised question to stdout' do
-      expect(outputting_the_question).to \
-        output(colourised_translated_message).to_stdout
-    end
-  end
+    describe '.error' do
+      include_context 'I18n keys', :red
 
-  describe '.success' do
-    include_context 'I18n keys', :green
+      let(:outputting_the_error) { -> { described_class.error(params) } }
 
-    let(:outputting_the_success_message) do
-      -> { described_class.success(params) }
+      it 'outputs the values of the I18n-ised error to stdout' do
+        expect(outputting_the_error).to \
+          output(colourised_translated_message_with_new_line).to_stdout
+      end
     end
 
-    it 'outputs the values of the I18n-ised success message to stdout' do
-      expect(outputting_the_success_message).to \
-        output(colourised_translated_message_with_new_line).to_stdout
-    end
-  end
+    describe '.warning' do
+      include_context 'I18n keys', :yellow
 
-  describe '.info' do
-    include_context 'I18n keys', :cyan
+      let(:outputting_the_warning) do
+        -> { described_class.warning(params) }
+      end
 
-    let(:outputting_the_info_message) do
-      -> { described_class.info(params) }
-    end
-
-    it 'outputs the values of the I18n-ised info message to stdout' do
-      expect(outputting_the_info_message).to \
-        output(colourised_translated_message_with_new_line).to_stdout
-    end
-  end
-
-  describe '.plain' do
-    let(:key) { :message }
-    let(:params_to_interpolate) { { param: 'param' } }
-    let(:params) { [key, params_to_interpolate] }
-    let(:translated_message) { 'Translated Message' }
-    let(:translated_message_with_new_line) { "#{translated_message}\n" }
-    let(:outputting_the_plain_message) do
-      -> { described_class.plain(params) }
+      it 'outputs the values of the I18n-ised warning to stdout' do
+        expect(outputting_the_warning).to \
+          output(colourised_translated_message_with_new_line).to_stdout
+      end
     end
 
-    before do
-      allow(I18n).to receive(:translate).
-        with(key, params_to_interpolate).and_return(translated_message)
+    describe '.question' do
+      include_context 'I18n keys', :yellow
+
+      let(:outputting_the_question) do
+        -> { described_class.question(params) }
+      end
+
+      it 'outputs the values of the I18n-ised question to stdout' do
+        expect(outputting_the_question).to \
+          output(colourised_translated_message).to_stdout
+      end
     end
 
-    it 'outputs the values of the I18n-ised plain message to stdout' do
-      expect(outputting_the_plain_message).to \
-        output(translated_message_with_new_line).to_stdout
-    end
-  end
+    describe '.success' do
+      include_context 'I18n keys', :green
 
-  describe '.raw' do
-    let(:message) { 'The message' }
-    let(:message_with_new_line) { "#{message}\n" }
-    let(:outputting_the_raw_message) do
-      -> { described_class.raw(message) }
+      let(:outputting_the_success_message) do
+        -> { described_class.success(params) }
+      end
+
+      it 'outputs the values of the I18n-ised success message to stdout' do
+        expect(outputting_the_success_message).to \
+          output(colourised_translated_message_with_new_line).to_stdout
+      end
     end
 
-    it 'outputs the values of the message to stdout' do
-      expect(outputting_the_raw_message).to \
-        output(message_with_new_line).to_stdout
+    describe '.info' do
+      include_context 'I18n keys', :cyan
+
+      let(:outputting_the_info_message) do
+        -> { described_class.info(params) }
+      end
+
+      it 'outputs the values of the I18n-ised info message to stdout' do
+        expect(outputting_the_info_message).to \
+          output(colourised_translated_message_with_new_line).to_stdout
+      end
+    end
+
+    describe '.plain' do
+      let(:key) { :message }
+      let(:params_to_interpolate) { { param: 'param' } }
+      let(:params) { [key, params_to_interpolate] }
+      let(:translated_message) { 'Translated Message' }
+      let(:translated_message_with_new_line) { "#{translated_message}\n" }
+      let(:outputting_the_plain_message) do
+        -> { described_class.plain(params) }
+      end
+
+      before do
+        allow(I18n).to receive(:translate).
+          with(key, params_to_interpolate).and_return(translated_message)
+      end
+
+      it 'outputs the values of the I18n-ised plain message to stdout' do
+        expect(outputting_the_plain_message).to \
+          output(translated_message_with_new_line).to_stdout
+      end
+    end
+
+    describe '.raw' do
+      let(:message) { 'The message' }
+      let(:message_with_new_line) { "#{message}\n" }
+      let(:outputting_the_raw_message) do
+        -> { described_class.raw(message) }
+      end
+
+      it 'outputs the values of the message to stdout' do
+        expect(outputting_the_raw_message).to \
+          output(message_with_new_line).to_stdout
+      end
     end
   end
 end
