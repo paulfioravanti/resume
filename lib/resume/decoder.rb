4b9821e1ce0461ea75a62fd5a8fd34aa031ff754
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-08-30 21:14:51 +1000

    Remove old comment

--- lib/resume/decoder.rb
+++ lib/resume/decoder.rb
@@ -2,8 +2,6 @@ require 'base64'
 
 module Resume
   class Decoder
-    # This is just a helper method due to the sheer amount of
-    # decoding that occurs throughout the code
     def self.decode(string)
       # Force encoding to UTF-8 is needed for strings that had UTF-8
       # characters in them when they were originally encoded
