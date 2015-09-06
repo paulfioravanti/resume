8af9449081dc0577f1e7e71c0ae06b98c127f344
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-09-06 22:21:02 +1000

    Change repo to point at master

--- lib/resume.rb
+++ lib/resume.rb
@@ -3,8 +3,7 @@ require_relative 'resume/cli/application'
 
 module Resume
   VERSION = '1.0'
-  # REMOTE_REPO = "https://raw.githubusercontent.com/paulfioravanti/resume/master"
-  REMOTE_REPO = "https://raw.githubusercontent.com/paulfioravanti/resume/ja-resume-refactor"
+  REMOTE_REPO = "https://raw.githubusercontent.com/paulfioravanti/resume/master"
 
   def self.generate
     CLI::Application.start
