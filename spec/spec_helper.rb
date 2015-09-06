ee424c410d0a75886aff4707c6ef8c7ef85cfd4d
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-09-06 15:31:15 +1000

    Add support for :focus in the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ require 'resume/output'
 
 module Resume
   RSpec.configure do |config|
+    config.filter_run focus: true
+    config.run_all_when_everything_filtered = true
     config.disable_monkey_patching!
     config.before(:suite) do
       begin
