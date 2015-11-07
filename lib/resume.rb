083ec8ce9b766e58e126b2e0ba1b892efcaf3f99
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-11-08 10:01:58 +1100

    Move reserved words constant into Resume module

--- lib/resume.rb
+++ lib/resume.rb
@@ -3,6 +3,8 @@ require_relative 'resume/cli/application'
 module Resume
   VERSION = '1.0'
   REMOTE_REPO = 'https://raw.githubusercontent.com/paulfioravanti/resume/master'
+  # Specific 4-letter words to be excluded from Base64 decoding
+  RESERVED_WORDS = ['bold', 'left']
 
   def self.generate
     CLI::Application.start
