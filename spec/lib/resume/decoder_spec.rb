b154a0ef056208f5778d24ba6fafdf84932a6603
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-11-08 10:43:58 +1100

    Re-do decoder specs

--- spec/lib/resume/decoder_spec.rb
+++ spec/lib/resume/decoder_spec.rb
@@ -2,20 +2,92 @@ require 'resume/decoder'
 
 module Resume
   RSpec.describe Decoder do
-    let(:encoded_string) { '3|\|c0D3d 1N|=0' }
-    let(:decoded_string) { 'Encoded info' }
+    describe '.decode' do
+      let(:decode_object) { described_class.decode.call(object) }
 
-    before do
-      allow(Base64).to \
-        receive(:strict_decode64).with(encoded_string).
-          and_return(decoded_string)
+      context 'when object is a Hash' do
+        let(:object) { { key: value } }
+
+        context 'when hash value is a Base64 string' do
+          let(:decoded_object) { { key: decoded_value } }
+
+          context 'when hash value is not a reserved word' do
+            let(:value) { 'RW5jb2RlZA==' }
+            let(:decoded_value) { 'Encoded' }
+
+            it 'decodes the hash value' do
+              expect(decode_object).to eq(decoded_object)
+            end
+          end
+
+          context 'when hash value is a reserved word' do
+            let(:value) { 'bold' }
+            let(:decoded_value) { value }
+
+            before do
+              stub_const('Resume::RESERVED_WORDS', [value])
+            end
+
+            it 'returns the object unchanged' do
+              expect(decode_object).to eq(decoded_object)
+            end
+          end
+        end
+
+        context 'when hash value is not a Base64 string' do
+          let(:value) { 'A string' }
+
+          it 'returns the object unchanged' do
+            expect(decode_object).to eq(object)
+          end
+        end
+      end
+
+      context 'when object is an Array' do
+        let(:object) { [value] }
+
+        context 'when array value is a Base64 string' do
+          let(:decoded_object) { [decoded_value] }
+
+          context 'when array value is not a reserved word' do
+            let(:value) { 'RW5jb2RlZA==' }
+            let(:decoded_value) { 'Encoded' }
+
+            it 'decodes the array value' do
+              expect(decode_object).to eq(decoded_object)
+            end
+          end
+
+          context 'when array value is a reserved word' do
+            let(:value) { 'bold' }
+            let(:decoded_value) { value }
+
+            before do
+              stub_const('Resume::RESERVED_WORDS', [value])
+            end
+
+            it 'returns the object unchanged' do
+              expect(decode_object).to eq(decoded_object)
+            end
+          end
+        end
+
+        context 'when array value is not a Base64 string' do
+          let(:value) { 'A string' }
+
+          it 'returns the object unchanged' do
+            expect(decode_object).to eq(object)
+          end
+        end
+      end
+
+      context 'when object is neither a Hash nor an Array' do
+        let(:object) { 'A string' }
+
+        it 'returns the object unchanged' do
+          expect(decode_object).to eq(object)
+        end
+      end
     end
-
-    # describe '.decode' do
-    #   it 'strictly decodes the string, forcing UTF-8 encoding' do
-    #     expect(decoded_string).to receive(:force_encoding).with('utf-8')
-    #     described_class.decode(encoded_string)
-    #   end
-    # end
   end
 end
