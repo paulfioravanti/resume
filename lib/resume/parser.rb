ef0134b12c5762496e41d2d76cb5e3e68f16cddf
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-11-12 08:50:56 +1100

    Make the parser methods class methods again as it doesn't need to hold state

--- lib/resume/parser.rb
+++ lib/resume/parser.rb
@@ -5,40 +5,21 @@ require_relative 'file_fetcher'
 
 module Resume
   class Parser
-    def self.parse(resume)
-      new(resume).parse
-    end
-
     def self.decode_content(string)
       # Force encoding to UTF-8 is needed for strings that had UTF-8
       # characters in them when they were originally encoded
       Base64.strict_decode64(string).force_encoding('utf-8')
     end
 
-    private_class_method :new
-
-    def initialize(resume)
-      @resume = resume
-    end
-
-    def parse
-      parse_resume
-      resume
-    end
-
-    private
-
-    attr_accessor :resume
-
-    def parse_resume
-      self.resume = JSON.recurse_proc(resume, &decode_and_fetch_assets)
+    def self.parse(resume)
+      JSON.recurse_proc(resume, &decode_and_fetch_assets)
     end
 
     # Base64-encoded values can be found in hash and array values
     # in the JSON, so specifically target those data types for
     # manipulation, and ignore any direct references given to the
     # keys or values of the JSON hash.
-    def decode_and_fetch_assets
+    def self.decode_and_fetch_assets
       Proc.new do |object|
         case object
         when Hash
@@ -69,26 +50,29 @@ module Resume
         end
       end
     end
+    private_class_method :decode_and_fetch_assets
 
-
-    def encoded?(string)
+    def self.encoded?(string)
       # Checking whether a string is Base64-encoded is not an
       # exact science: 4 letter English words can be construed
       # as being encoded, so logic is needed to exclude words
       # known to not be encoded.
       base64_string?(string) && !RESERVED_WORDS.include?(string)
     end
+    private_class_method :encoded?
 
     # Taken from http://stackoverflow.com/q/8571501/567863
-    def base64_string?(string)
+    def self.base64_string?(string)
       string =~ %r{\A
         ([A-Za-z0-9+/]{4})*
         ([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)
       \z}x
     end
+    private_class_method :base64_string?
 
-    def asset?(string)
+    def self.asset?(string)
       string =~ %r{dropbox}
     end
+    private_class_method :asset?
   end
 end
