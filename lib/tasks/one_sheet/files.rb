4d8b98d183e058880d3403bbe7d08eef15bdfe19
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-04-25 22:29:28 +1000

    Make the files.rb file much smaller

--- lib/tasks/one_sheet/files.rb
+++ lib/tasks/one_sheet/files.rb
@@ -1,9 +1,9 @@
 module OneSheet
   class Files
-    extend Forwardable
-
     def self.read(type)
-      new(type).read
+      type[:files].reduce('') do |content, file|
+        content << read_file(type[:path], file)
+      end
     end
 
     def self.read_file(path, file)
@@ -11,24 +11,5 @@ module OneSheet
       lines[file[:from_line]..file[:to_line]].join << "\n"
     end
     private_class_method :read_file
-
-    private_class_method :new
-
-    def initialize(type)
-      @files = type[:files]
-      @path = type[:path]
-    end
-
-    def_delegator self, :read_file
-
-    def read
-      files.reduce('') do |content, file|
-        content << read_file(path, file)
-      end
-    end
-
-    private
-
-    attr_reader :path, :files
   end
 end
