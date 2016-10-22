c1e1abff55be6b65625a9b0959744a92a0564602
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-22 15:26:12 +1100

    Double quote strings in colours and output spec

--- spec/lib/resume/colours_spec.rb
+++ spec/lib/resume/colours_spec.rb
@@ -2,40 +2,40 @@ require 'resume/colours'
 
 module Resume
   RSpec.describe Colours do
-    let(:text) { 'Hello' }
+    let(:text) { "Hello" }
 
-    describe '.red' do
+    describe ".red" do
       let(:red_text) { described_class.red(text) }
       let(:ansi_escaped_red_text) { "\e[31m#{text}\e[0m" }
 
-      it 'returns the text formatted to output in red on terminals' do
+      it "returns the text formatted to output in red on terminals" do
         expect(red_text).to eq(ansi_escaped_red_text)
       end
     end
 
-    describe '.green' do
+    describe ".green" do
       let(:green_text) { described_class.green(text) }
       let(:ansi_escaped_green_text) { "\e[32m#{text}\e[0m" }
 
-      it 'returns the text formatted to output in green on terminals' do
+      it "returns the text formatted to output in green on terminals" do
         expect(green_text).to eq(ansi_escaped_green_text)
       end
     end
 
-    describe '.yellow' do
+    describe ".yellow" do
       let(:yellow_text) { described_class.yellow(text) }
       let(:ansi_escaped_yellow_text) { "\e[33m#{text}\e[0m" }
 
-      it 'returns the text formatted to output in yellow on terminals' do
+      it "returns the text formatted to output in yellow on terminals" do
         expect(yellow_text).to eq(ansi_escaped_yellow_text)
       end
     end
 
-    describe '.cyan' do
+    describe ".cyan" do
       let(:cyan_text) { described_class.cyan(text) }
       let(:ansi_escaped_cyan_text) { "\e[36m#{text}\e[0m" }
 
-      it 'returns the text formatted to output in cyan on terminals' do
+      it "returns the text formatted to output in cyan on terminals" do
         expect(cyan_text).to eq(ansi_escaped_cyan_text)
       end
     end
--- spec/lib/resume/output_spec.rb
+++ spec/lib/resume/output_spec.rb
@@ -3,11 +3,11 @@ require 'resume/output'
 
 module Resume
   RSpec.describe Output do
-    shared_context 'Colourised I18n keys' do |colour|
+    shared_context "Colourised I18n keys" do |colour|
       let(:key) { :message }
-      let(:params_to_interpolate) { { param: 'param' } }
+      let(:params_to_interpolate) { { param: "param" } }
       let(:params) { [key, params_to_interpolate] }
-      let(:translated_message) { 'Translated Message' }
+      let(:translated_message) { "Translated Message" }
       let(:colourised_translated_message) do
         "Colourised #{translated_message}"
       end
@@ -25,8 +25,8 @@ module Resume
       end
     end
 
-    shared_context 'Raw colourised strings' do |colour|
-      let(:message) { 'The message' }
+    shared_context "Raw colourised strings" do |colour|
+      let(:message) { "The message" }
       let(:colourised_message) do
         "Colourised #{message}"
       end
@@ -41,10 +41,10 @@ module Resume
       end
     end
 
-    describe '.messages' do
+    describe ".messages" do
       let(:messages) { { error: :foo, warning: :bar } }
 
-      it 'calls to output each message as its specific type' do
+      it "calls to output each message as its specific type" do
         messages.each do |type, key|
           expect(described_class).to receive(type).with(key)
         end
@@ -52,74 +52,74 @@ module Resume
       end
     end
 
-    describe '.error' do
-      include_context 'Colourised I18n keys', :red
+    describe ".error" do
+      include_context "Colourised I18n keys", :red
 
       let(:outputting_the_error) { -> { described_class.error(params) } }
 
-      it 'outputs the values of the I18n-ised error to stdout' do
+      it "outputs the values of the I18n-ised error to stdout" do
         expect(outputting_the_error).to \
           output(colourised_translated_message_with_new_line).to_stdout
       end
     end
 
-    describe '.warning' do
-      include_context 'Colourised I18n keys', :yellow
+    describe ".warning" do
+      include_context "Colourised I18n keys", :yellow
 
       let(:outputting_the_warning) do
         -> { described_class.warning(params) }
       end
 
-      it 'outputs the values of the I18n-ised warning to stdout' do
+      it "outputs the values of the I18n-ised warning to stdout" do
         expect(outputting_the_warning).to \
           output(colourised_translated_message_with_new_line).to_stdout
       end
     end
 
-    describe '.question' do
-      include_context 'Colourised I18n keys', :yellow
+    describe ".question" do
+      include_context "Colourised I18n keys", :yellow
 
       let(:outputting_the_question) do
         -> { described_class.question(params) }
       end
 
-      it 'outputs the values of the I18n-ised question to stdout' do
+      it "outputs the values of the I18n-ised question to stdout" do
         expect(outputting_the_question).to \
           output(colourised_translated_message).to_stdout
       end
     end
 
-    describe '.success' do
-      include_context 'Colourised I18n keys', :green
+    describe ".success" do
+      include_context "Colourised I18n keys", :green
 
       let(:outputting_the_success_message) do
         -> { described_class.success(params) }
       end
 
-      it 'outputs the values of the I18n-ised success message to stdout' do
+      it "outputs the values of the I18n-ised success message to stdout" do
         expect(outputting_the_success_message).to \
           output(colourised_translated_message_with_new_line).to_stdout
       end
     end
 
-    describe '.info' do
-      include_context 'Colourised I18n keys', :cyan
+    describe ".info" do
+      include_context "Colourised I18n keys", :cyan
 
       let(:outputting_the_info_message) do
         -> { described_class.info(params) }
       end
 
-      it 'outputs the values of the I18n-ised info message to stdout' do
+      it "outputs the values of the I18n-ised info message to stdout" do
         expect(outputting_the_info_message).to \
           output(colourised_translated_message_with_new_line).to_stdout
       end
     end
 
-    describe '.plain' do
+    describe ".plain" do
       let(:key) { :message }
-      let(:params_to_interpolate) { { param: 'param' } }
+      let(:params_to_interpolate) { { param: "param" } }
       let(:params) { [key, params_to_interpolate] }
-      let(:translated_message) { 'Translated Message' }
+      let(:translated_message) { "Translated Message" }
       let(:translated_message_with_new_line) { "#{translated_message}\n" }
       let(:outputting_the_plain_message) do
         -> { described_class.plain(params) }
@@ -130,59 +130,59 @@ module Resume
           with(key, params_to_interpolate).and_return(translated_message)
       end
 
-      it 'outputs the values of the I18n-ised plain message to stdout' do
+      it "outputs the values of the I18n-ised plain message to stdout" do
         expect(outputting_the_plain_message).to \
           output(translated_message_with_new_line).to_stdout
       end
     end
 
-    describe '.raw_error' do
-      include_context 'Raw colourised strings', :red
+    describe ".raw_error" do
+      include_context "Raw colourised strings", :red
 
       let(:outputting_the_raw_error) do
         -> { described_class.raw_error(message) }
       end
 
-      it 'outputs the colourised error message to stdout' do
+      it "outputs the colourised error message to stdout" do
         expect(outputting_the_raw_error).to \
           output(message_with_new_line).to_stdout
       end
     end
 
-    describe '.raw_warning' do
-      include_context 'Raw colourised strings', :yellow
+    describe ".raw_warning" do
+      include_context "Raw colourised strings", :yellow
 
       let(:outputting_the_raw_warning) do
         -> { described_class.raw_warning(message) }
       end
 
-      it 'outputs the colourised warning message to stdout' do
+      it "outputs the colourised warning message to stdout" do
         expect(outputting_the_raw_warning).to \
           output(message_with_new_line).to_stdout
       end
     end
 
-    describe '.raw_success' do
-      include_context 'Raw colourised strings', :green
+    describe ".raw_success" do
+      include_context "Raw colourised strings", :green
 
       let(:outputting_the_raw_success_message) do
         -> { described_class.raw_success(message) }
       end
 
-      it 'outputs the colourised warning message to stdout' do
+      it "outputs the colourised warning message to stdout" do
         expect(outputting_the_raw_success_message).to \
           output(message_with_new_line).to_stdout
       end
     end
 
-    describe '.raw' do
-      let(:message) { 'The message' }
+    describe ".raw" do
+      let(:message) { "The message" }
       let(:message_with_new_line) { "#{message}\n" }
       let(:outputting_the_raw_message) do
         -> { described_class.raw(message) }
       end
 
-      it 'outputs the values of the message to stdout' do
+      it "outputs the values of the message to stdout" do
         expect(outputting_the_raw_message).to \
           output(message_with_new_line).to_stdout
       end
