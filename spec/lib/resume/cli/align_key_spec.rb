f512ca158961099251a187980a9a829f1c2ba119
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-03-23 09:08:32 +1100

    Minor rewrite of spec

--- spec/lib/resume/cli/align_key_spec.rb
+++ spec/lib/resume/cli/align_key_spec.rb
@@ -4,7 +4,7 @@ module Resume
   module CLI
     RSpec.describe AlignKey do
       describe ".===" do
-        context "when other has a length more than 1" do
+        context "when other has a length > 1" do
           let(:long_hash) { { align: 1, extra: 2 } }
 
           it "returns false" do
@@ -12,19 +12,19 @@ module Resume
           end
         end
 
-        context "when other has a length of 1 and has an align key" do
-          let(:align_key) { { align: 1 } }
-
-          it "returns true" do
-            expect(described_class === align_key).to be true
-          end
-        end
-
-        context "when other does not have an align key" do
+        context "when other has length == 1 but key != :align" do
           let(:non_align_key) { { something_else: 1 } }
 
           it "returns false" do
             expect(described_class === non_align_key).to be false
+          end
+        end
+
+        context "when other has length == 1 and key == :align" do
+          let(:align_key) { { align: 1 } }
+
+          it "returns true" do
+            expect(described_class === align_key).to be true
           end
         end
       end
