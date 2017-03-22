c0b10735988f0a20dac2014795bb6275c42ac6b5
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-03-22 22:41:36 +1100

    Refactor social media logo set code

--- lib/resume/pdf/social_media_logo_set.rb
+++ lib/resume/pdf/social_media_logo_set.rb
@@ -7,9 +7,7 @@ module Resume
 
       def generate(pdf, logo_set)
         top_padding, horizontal_rule_colour =
-          logo_set.values_at(
-            :top_padding, :bottom_padding, :horizontal_rule_colour
-          )
+          logo_set.values_at(:top_padding, :horizontal_rule_colour)
         pdf.move_down(top_padding)
         generate_logos(pdf, logo_set)
         pdf.stroke_horizontal_rule { color(horizontal_rule_colour) }
@@ -18,9 +16,7 @@ module Resume
       def generate_logos(pdf, logo_set)
         logos = entire_logo_properties(logo_set)
         generate_logo_for(logos.first, pdf, logo_set)
-        logos[1..-1].each do |logo|
-          generate_logo_for(logo, pdf, logo_set)
-        end
+        generate_logos_for(logos[1..-1], pdf, logo_set)
         pdf.move_down(logo_set[:bottom_padding])
       end
       private_class_method :generate_logos
@@ -33,7 +29,6 @@ module Resume
       private_class_method :entire_logo_properties
 
       def generate_logo_for(logo, pdf, logo_set)
-        pdf.move_up(logo_set[:padded_logo_height])
         x_position = logo_set[:x_position]
         pdf.bounding_box([x_position, pdf.cursor], width: logo[:width]) do
           ImageLink.generate(pdf, logo)
@@ -42,6 +37,14 @@ module Resume
         logo_set[:x_position] = x_position + logo_set[:padded_logo_width]
       end
       private_class_method :generate_logo_for
+
+      def generate_logos_for(logos, pdf, logo_set)
+        logos.each do |logo|
+          pdf.move_up(logo_set[:padded_logo_height])
+          generate_logo_for(logo, pdf, logo_set)
+        end
+      end
+      private_class_method :generate_logos
     end
   end
 end
