433a4731b38a5ff603779131d04994012908448b
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-03-23 21:26:02 +1100

    Refactor file fetchers to be functional

--- lib/resume/cli/resume_data_fetcher.rb
+++ lib/resume/cli/resume_data_fetcher.rb
@@ -4,14 +4,12 @@ require_relative "file_fetcher"
 
 module Resume
   module CLI
-    class ResumeDataFetcher < FileFetcher
-      def self.fetch
-        super("resources/resume.#{I18n.locale}.json")
-      end
+    module ResumeDataFetcher
+      module_function
 
       def fetch
         Output.plain(:gathering_resume_information)
-        resume = super
+        resume = FileFetcher.fetch("resources/resume.#{I18n.locale}.json")
         JSON.parse(resume.read, symbolize_names: true)
       end
     end
