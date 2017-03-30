b598aef9b5ac3a7abd8327d7b49cae84d27b3a55
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-03-31 10:02:23 +1100

    Add first yardoc

--- lib/resume.rb
+++ lib/resume.rb
@@ -1,8 +1,18 @@
+# Top level module for resume CLI utility and PDF generation
+#
+# @author Paul Fioravanti
 module Resume
   VERSION = "1.3".freeze
 
   module_function
 
+  # Entry point for generating the PDF resume.
+  #
+  # Ensures that correct version of Ruby is being used before
+  # attempting to generate it.
+  #
+  # @author Paul Fioravanti
+  # @return [nil]
   def generate
     require_relative "resume/ruby_version_checker"
     RubyVersionChecker.check_ruby_version
