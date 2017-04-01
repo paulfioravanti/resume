5f9887bff25bb498e12a747b97d1a95fa83f8f99
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-04-01 16:16:05 +1100

    Document the content parser, errors, dependency manager and exception suppressor

--- lib/resume/cli/dependency_manager.rb
+++ lib/resume/cli/dependency_manager.rb
@@ -6,9 +6,18 @@ require_relative "font_downloader"
 
 module Resume
   module CLI
+    # Class concerned with managing the dependencies that generation
+    # of the resume requires, which can vary depending on the resume's locale.
+    #
+    # @author Paul Fioravanti
     class DependencyManager
       extend Forwardable
 
+      # Initialises a new instance of a Dependency Manager.
+      #
+      # @param dependencies [Hash]
+      #   A hash containing the gem and font dependencies for resume generation.
+      # @return [DependencyManager] The dependency manager object.
       def initialize(dependencies)
         @gem_installer = GemInstaller.new(dependencies[:gems])
         @font_downloader = FontDownloader.new(dependencies[:fonts])
@@ -26,12 +35,21 @@ module Resume
                      :output_font_dependencies,
                      :fonts_successfully_downloaded?
 
+      # Determines whether gem or font dependencies are required to
+      # generate resume.
+      #
+      # @return [true] if installation of dependencies required.
+      # @return [false] if installation of dependencies not required.
       def installation_required?
         audit_gem_dependencies
         audit_font_dependencies
         dependencies_present?
       end
 
+      # Asks the user generating the resume for permission to install
+      # the required dependencies.
+      #
+      # @return [nil]
       def request_dependency_installation
         Output.warning(:i_need_the_following_to_generate_a_pdf)
         output_gem_dependencies
@@ -39,6 +57,13 @@ module Resume
         Output.question(:may_i_please_install_them)
       end
 
+      # Attempts to install the dependencies needed to
+      # generate the resume.
+      #
+      # @raise [DependencyInstallationError]
+      #   if any dependencies are unable to be installed.
+      # @return [nil]
+      #   if all dependencies are able to be installed.
       def install
         # rubocop:disable Style/GuardClause
         # NOTE: I think a non-guard clause reads better here
--- lib/resume/cli/exception_suppressor.rb
+++ lib/resume/cli/exception_suppressor.rb
@@ -1,14 +1,24 @@
 module Resume
   module CLI
+    # Module concerned with suppressing exceptions of a specific type.
+    #
+    # @author Paul Fioravanti
     module ExceptionSuppressor
       module_function
 
-      # rubocop:disable Lint/RescueException
-      # NOTE: This method needs to rescue from LoadError and Gem::LoadError
-      # which don't inherit from StandardError, hence needing to rescue from
-      # the Exception class.
+      # Suppresses exception of type `exception_to_ignore` and `call`s
+      # code contained inside `default` if exception is to be ignored.
+      #
+      # @param exception_to_ignore [Exception]
+      #   The exception type to ignore.
+      # @param default [Proc]
+      #   The code to be executed when the exception is to be ignored.
       def suppress(exception_to_ignore = StandardError, default = -> {})
+        # NOTE: This method needs to rescue from LoadError and Gem::LoadError
+        # which don't inherit from StandardError, hence needing to rescue from
+        # the Exception class.
         yield
+      # rubocop:disable Lint/RescueException
       rescue Exception => exception
         raise unless exception.is_a?(exception_to_ignore)
         default.call
