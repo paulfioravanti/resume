1ec1c6d69488364d796269280bc5f7d25f3febef
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-04-01 20:20:07 +1100

    Document the ResumeDataFetcher

--- lib/resume/cli/resume_data_fetcher.rb
+++ lib/resume/cli/resume_data_fetcher.rb
@@ -4,9 +4,16 @@ require_relative "file_fetcher"
 
 module Resume
   module CLI
+    # Module concerned with fetching and parsing a resume data JSON file.
+    #
+    # @author Paul Fioravanti
     module ResumeDataFetcher
       module_function
 
+      # Fetches the resume data JSON file for the current locale
+      # and parses it into a Ruby hash.
+      #
+      # @return [Hash] The resume data
       def fetch
         Output.plain(:gathering_resume_information)
         resume = FileFetcher.fetch("resources/resume.#{I18n.locale}.json")
