c9462f32dc251889c9284f8b2e542df68a891363
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-04-01 14:24:29 +1100

    Document the Colours module

--- lib/resume/colours.rb
+++ lib/resume/colours.rb
@@ -1,19 +1,38 @@
 module Resume
+  # Module representing CLI colour output.
+  #
+  # @author Paul Fioravanti
   module Colours
     module_function
 
+    # Wraps `text` in ASCII escape codes for red text.
+    #
+    # @param text [String] the text to output
+    # @return [String] the colour ASCII-escaped string
     def red(text)
       colourize(text, colour_code: 31)
     end
 
+    # Wraps `text` in ASCII escape codes for green text.
+    #
+    # @param (see #red)
+    # @return (see #red)
     def green(text)
       colourize(text, colour_code: 32)
     end
 
+    # Wraps `text` in ASCII escape codes for yellow text.
+    #
+    # @param (see #red)
+    # @return (see #red)
     def yellow(text)
       colourize(text, colour_code: 33)
     end
 
+    # Wraps `text` in ASCII escape codes for cyan text.
+    #
+    # @param (see #red)
+    # @return (see #red)
     def cyan(text)
       colourize(text, colour_code: 36)
     end
