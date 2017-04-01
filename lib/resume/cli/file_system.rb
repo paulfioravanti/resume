b1a93f118c41b036633892dcede4c03a1f5d5fc0
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-04-01 19:50:57 +1100

    Add documentation for FileSystem

--- lib/resume/cli/file_system.rb
+++ lib/resume/cli/file_system.rb
@@ -3,9 +3,19 @@ require "pathname"
 
 module Resume
   module CLI
+    # Module containing functions concerning interacting with the
+    # local file system.
+    #
+    # @author Paul Fioravanti
     module FileSystem
       module_function
 
+      # Attempts to open a file in a system-appropriate way.
+      #
+      # @param filename [String] The filename to open.
+      # @return [true] if the document can be opened successfully.
+      # @return [false] if the document cannot be opened successfully.
+      # @return [nil] if it is unknown how to attempt to open the document.
       def open_document(filename)
         case RUBY_PLATFORM
         when /darwin/
@@ -19,6 +29,10 @@ module Resume
         end
       end
 
+      # Derive a system-independent tmpfile path from a `filename`.
+      #
+      # @param filename [String] The filename to create a tmpfile path for.
+      # @return [Pathname] The generated tmpfile pathname for `filename`.
       def tmpfile_path(filename)
         # Ensure that the ?dl=1 parameter is removed
         Pathname.new(Dir.tmpdir).join(filename.sub(/\?.+\z/, ""))
