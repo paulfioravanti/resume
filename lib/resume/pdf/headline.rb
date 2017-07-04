e7b248cf6d54f8c2d74336e6345c01d8a2776e8a
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-07-05 09:36:28 +1000

    Remove explicit 'Ruby' reference from headline

--- lib/resume/pdf/headline.rb
+++ lib/resume/pdf/headline.rb
@@ -15,17 +15,7 @@ module Resume
       #   Presentation information about the headline.
       def generate(pdf, headline)
         pdf.move_down(headline[:top_padding])
-        primary_header = headline[:primary]
-        pdf.formatted_text(
-          [
-            {
-              text: primary_header[:text],
-              color: primary_header[:colour]
-            },
-            { text: headline.dig(:secondary, :text) }
-          ],
-          size: headline[:size]
-        )
+        pdf.text(headline[:text], size: headline[:size])
       end
     end
   end
