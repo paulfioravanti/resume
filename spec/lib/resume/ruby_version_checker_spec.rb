713330038898d5a8a3f484ed30cdd643ba78f4f2
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-09-18 14:29:04 +1000

    Change Ruby-related strings in app to 2.4.2

--- spec/lib/resume/ruby_version_checker_spec.rb
+++ spec/lib/resume/ruby_version_checker_spec.rb
@@ -11,7 +11,7 @@ module Resume
       end
 
       before do
-        stub_const("#{described_class}::RUBY_VERSION", "2.4.1")
+        stub_const("#{described_class}::RUBY_VERSION", "2.4.2")
       end
 
       context "when a LoadError occurs when loading rubygems lib" do
