67899ea7e753d32b63cef3e6af2c08525cf6c3ae
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-09-19 12:02:54 +1000

    Wholesale delegate parameters that need to go to I18n.translate

--- lib/resume/output.rb
+++ lib/resume/output.rb
@@ -26,10 +26,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload error(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def error(key, **params)
-      puts Colours.red(I18n.translate(key, **params))
+    def error(...)
+      puts Colours.red(I18n.translate(...))
     end
 
     # Outputs the translated message for `params` to `$stdout` in yellow
@@ -39,10 +39,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload warning(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def warning(key, **params)
-      puts Colours.yellow(I18n.translate(key, **params))
+    def warning(...)
+      puts Colours.yellow(I18n.translate(...))
     end
 
     # Outputs the translated message for `params` to `$stdout` in yellow
@@ -52,10 +52,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload question(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def question(key, **params)
-      print Colours.yellow(I18n.translate(key, **params))
+    def question(...)
+      print Colours.yellow(I18n.translate(...))
     end
 
     # Outputs the translated message for `params` to `$stdout` in green
@@ -65,10 +65,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload success(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def success(key, **params)
-      puts Colours.green(I18n.translate(key, **params))
+    def success(...)
+      puts Colours.green(I18n.translate(...))
     end
 
     # Outputs the translated message for `params` to `$stdout` in cyan
@@ -78,10 +78,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload info(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def info(key, **params)
-      puts Colours.cyan(I18n.translate(key, **params))
+    def info(...)
+      puts Colours.cyan(I18n.translate(...))
     end
 
     # Outputs the translated message for `params` to `$stdout` in default
@@ -91,10 +91,10 @@ module Resume
     #   @param key [Symbol] The message key to translate
     # @overload plain(key, params)
     #   @param key [Symbol] The message key to translate
-    #   @param params [Hash] The params to pass in to the translation message
+    #   @param options [Hash] The options to pass in to the translation message
     # @return [nil]
-    def plain(key, **options)
-      puts I18n.translate(key, **options)
+    def plain(...)
+      puts I18n.translate(...)
     end
 
     # Outputs the `message` parameter to `$stdout` in red
