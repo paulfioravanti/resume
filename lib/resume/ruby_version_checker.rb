0f703947d74515f8ae818145af5408641b485d69
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-07-24 10:48:33 +1000

    Update Ruby version in checker

--- lib/resume/ruby_version_checker.rb
+++ lib/resume/ruby_version_checker.rb
@@ -5,15 +5,15 @@ module Resume
   # @author Paul Fioravanti
   module RubyVersionChecker
     # Required Ruby version for resume to work
-    REQUIRED_RUBY_VERSION = "3.1.2".freeze
+    REQUIRED_RUBY_VERSION = "3.2.2".freeze
     private_constant :REQUIRED_RUBY_VERSION
     # Ruby version command
     RUBY_VERSION_COMMAND = "ruby -v".freeze
     private_constant :RUBY_VERSION_COMMAND
     # Ruby version output regex.
-    # For example, this will extract `3.1.2` from a string like:
-    # ruby 3.1.2p20 (2022-04-12 revision 4491bb740a) [arm64-darwin21]
-    RUBY_VERSION_REGEX = /\Aruby ([\d.][^p]+)/
+    # For example, this will extract `3.2.2` from a string like:
+    # ruby 3.2.2 (2023-03-30 revision e51014f9c0) [arm64-darwin22]
+    RUBY_VERSION_REGEX = /\Aruby ([\d\.]+)/.freeze
     private_constant :RUBY_VERSION_REGEX
 
     module_function
